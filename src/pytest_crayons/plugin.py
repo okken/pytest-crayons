4058977cb1c0c142cd743e3ab58ba7bc46e56b7c
Brian Okken <1568356+okken@users.noreply.github.com>
2023-02-19 20:17:41 -0800

    formatting

--- src/pytest_crayons/plugin.py
+++ src/pytest_crayons/plugin.py
@@ -1,4 +1,3 @@
-
 import pytest
 from functools import partial
 
@@ -12,28 +11,35 @@ CYAN = "\x1b[36m"
 RESET = "\x1b[0m"
 BOLD = "\x1b[1m"
 
+
 def _color_print(color: str, func_name: str, text: str):
     print(f"{BOLD}{color}{func_name}: {text}{RESET}")
+
 
 @pytest.fixture()
 def red(request):
     return partial(_color_print, RED, request.node.name)
 
+
 @pytest.fixture()
 def green(request):
     return partial(_color_print, GREEN, request.node.name)
+
 
 @pytest.fixture()
 def yellow(request):
     return partial(_color_print, YELLOW, request.node.name)
 
+
 @pytest.fixture()
 def blue(request):
     return partial(_color_print, BLUE, request.node.name)
 
+
 @pytest.fixture()
 def magenta(request):
     return partial(_color_print, MAGENTA, request.node.name)
+
 
 @pytest.fixture()
 def cyan(request):
